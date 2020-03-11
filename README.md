1a80aafb0375c4e677896c049868b5f9e511449f
Kenny Murrell <kenneth.cutkomp@gmail.com>
2020-03-11 13:09:20 -0500

    UI added
    Added UI. See 4th entry on Readme for details.

--- README.md
+++ README.md
@@ -27,3 +27,13 @@ Holy cow it works! I had a lot of trouble getting the loop to stop. It wouldn't 
 After that, it kept looping because I was using the || (or) instead of && (and). Once I made that simple change, it finally would stop after either score hit 5. Then I just added the window alerts saying who won or tied. 
 
 I also cleaned it up a bit by removing some of the global variables I either wasn't using or were unnecessary and removed my comments to myself (they were mostly things like "WHY ISNT THIS WORKING" ect.) All in all im happy. Now onto adding the UI which im sure is going to involve changing a bunch of stuff that works. Boo. ha. 
+
+
+FOURTH (and hopefully final) Notes:
+
+The UI was sooo much more fun than doing this project with just the javascript console! I ended up not needing any loops at all so I completely removed it. Basically after a round it adds +1 to either score and just an if/else statement that says if either reaches 5 then its a win/lose. 
+
+The most difficult part was actually the CSS. I really struggled to get everything to line up like I wanted. I used flexbox as suggested but I can't help think there's a better way. 
+The bottom two boxes, the comp choice and score boxes, change as the games played and i'd really rather have them be static in size and I couldn't really figure out how to make that happen so I guess I may have to re-visit this project later to really make it perfect. 
+
+Overall, I am happy with it! The spongebob theme kind of happened by accident when the first image that popped up for "rock" was the boulder from one of the episodes and I thought that would be a fun theme!
--- javascript.js
+++ javascript.js
@@ -1,59 +1,122 @@
 //variables
 const choices = ['rock','paper','scissors'];
+const score =document.querySelector('#score');
+const lastRound =document.querySelector('#lastRound');
+const playerScore =document.querySelector('#playerScore');
+const computerScore =document.querySelector('#computerScore');
 
+//Image changes on hover
+//No longer needed but keeping for reference
+function bigImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+  }
 
-function computerPlay () {
-    let computerSelection = choices[Math.floor(Math.random() * choices.length)];
-    return computerSelection;
-}
-function userPlay () {
-    const playerSelection =prompt('Rock,paper,or scissors?');
-    let lPlayerSelection = playerSelection.toLowerCase();
-    return lPlayerSelection;
-}
-
-function playRound() {
-    let lPlayerSelection= userPlay();
-    let computerSelection=computerPlay();
-    if ((lPlayerSelection == 'rock' && computerSelection=='rock') ||
-     (lPlayerSelection == 'paper' && computerSelection=='paper') ||
-     (lPlayerSelection == 'scissors' && computerSelection=='scissors'))
-    {outcome= "Tie"} 
-    else if ((lPlayerSelection == 'rock' && computerSelection=='scissors') ||
-     (lPlayerSelection == 'paper' && computerSelection=='rock') ||
-     (lPlayerSelection == 'scissors' && computerSelection=='paper'))
-    {outcome= "Win"}
-    else if ((lPlayerSelection == 'rock' && computerSelection=='paper') ||
-     (lPlayerSelection == 'paper' && computerSelection=='scissors') ||
-     (lPlayerSelection == 'scissors' && computerSelection=='rock'))
-    {outcome= "Lose"}
-    else {outcome= "You gotta enter the right thing fool"}  
-      return outcome;
-      }
-
-
-function game () {
-        
-        let compScore = 0;
-        let userScore = 0;
-        while (compScore<5 && userScore <5) {
-            playRound();
-        if (outcome=='Lose') {
-            compScore++;
-        } 
-        else if (outcome=='Win') {
-            userScore++;
-        }
-        console.log ('Computer Score:'+compScore);
-        console.log('Your Score:'+userScore);
-        console.log (outcome);
+  function normalImg(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+  }
+//computer play image change function
+function compImageChange() {
+    let compSelection = choices[Math.floor(Math.random() * choices.length)];
+    if (compSelection=='rock'){
+    document.getElementById("compImg").src = "https://vignette.wikia.nocookie.net/spongebob/images/4/45/Pete_the_rock.png/revision/latest?cb=20141115223905";
+    } else if (compSelection=='paper'){
+        document.getElementById("compImg").src = 'https://vignette.wikia.nocookie.net/spongebob/images/b/b7/The_Paper_014.png/revision/latest?cb=20191111160748'
+    } else if (compSelection=='scissors') {    
+        document.getElementById("compImg").src = 'https://vignette.wikia.nocookie.net/spongebob/images/2/2a/Bikini_Bottom_Scissor_Works.png/revision/latest?cb=20150709014043'
     }
-        if (compScore<userScore) {
-            window.alert('YOU WIN!')
-        } else if (compScore>userScore) {
-            window.alert('YOU LOSE!')
-        } else {
-            window.alert('IT"S A TIE!');
-        }
+    return compSelection;
+    }
+
+    var compScore = 0;
+    var userScore = 0;
+//functions for playing the game
+function playerRock () {
+    let computerSelection=compImageChange();
+    if (computerSelection=='rock') 
+   {result= "Tie"} 
+   else if 
+    (computerSelection=='scissors')
+   {result= "Win";}
+   else if (computerSelection=='paper') 
+   {result= "Lose"}
+    
+   if (result=="Win") {
+       userScore++;
+   } else if (result=="Lose") {
+       compScore++;
+   }
+   if (userScore==5) {
+    window.alert('YOU WIN');
+    window.location.reload();
+} else if (compScore==5) {
+    window.alert('YOU LOSE!');
+    window.location.reload();
 }
-game();
+
+   lastRound.innerHTML = "Last Round Result:"+result;
+   playerScore.innerHTML = "Your Points"+userScore;
+   computerScore.innerHTML = "Computer Points:"+compScore;
+}
+
+function playerPaper () {
+    let computerSelection=compImageChange();
+    if (computerSelection=='paper') 
+   {result= "Tie"} 
+   else if 
+    (computerSelection=='rock')
+   {result= "Win";}
+   else if (computerSelection=='scissors') 
+   {result= "Lose"}
+    
+   if (result=="Win") {
+       userScore++;
+   } else if (result=="Lose") {
+       compScore++;
+   }
+   lastRound.innerHTML = "Last Round Result:"+result;
+   playerScore.innerHTML = "Your Points"+userScore;
+   computerScore.innerHTML = "Computer Points:"+compScore;
+   if (userScore==5) {
+    window.alert('YOU WIN');
+    window.location.reload();
+} else if (compScore==5) {
+    window.alert('YOU LOSE!');
+    window.location.reload();
+}
+
+}
+
+function playerScissors () {
+    let computerSelection=compImageChange();
+    if (computerSelection=='scissors') 
+   {result= "Tie"} 
+   else if 
+    (computerSelection=='paper')
+   {result= "Win";}
+   else if (computerSelection=='rock') 
+   {result= "Lose"}
+    
+   if (result=="Win") {
+       userScore++;
+   } else if (result=="Lose") {
+       compScore++;
+   }
+   if (userScore==5) {
+    window.alert('YOU WIN');
+    window.location.reload();
+} else if (compScore==5) {
+    window.alert('YOU LOSE!');
+    window.location.reload();
+}
+
+   lastRound.innerHTML = "Last Round Result:"+result;
+   playerScore.innerHTML = "Your Points"+userScore;
+   computerScore.innerHTML = "Computer Points:"+compScore;
+}
+
+
+
+
+
